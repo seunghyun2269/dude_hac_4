1ec0cb8dc4e75a1ffc5232594d07cc94402a3bde
JungTag <dbsdudxor121@naver.com>
2020-07-23 12:42:54 +0900

    Add post views

--- proj/post/views.py
+++ proj/post/views.py
@@ -4,4 +4,10 @@ from .models import Post, Comment
 from accounts.models import Signup
 # Create your views here.
 
-def main()
+def home(request):
+    if request.method = "POST":
+        search = request.POST["search"]
+        post_list = Post.objects.filter(title = search)
+    else:
+        post_list = Post.objects.all()
+
