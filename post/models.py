286d91e75a029e23e89c81c8bd45be7131e70f71
yunjung <you@example.com>
2020-07-23 16:01:05 +0900

    all reset

---
+++ post/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from accounts.models import Signup
+from django.contrib.auth.models import User
+# Create your models here.
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='images/')
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+    pub_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.user
---
+++ post/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+from django.urls import path
+from post import views
+
+urlpatterns = [
+    path('home/',views.home,name="home"),
+    path('profile/<int:id>',views.profile, name="profile"),
+    path('detail/<int:id>', views.detail, name="detail"),
+    path('create', views.create, name="create"),
+    path('delete', views.delete, name="delete"),
+]
