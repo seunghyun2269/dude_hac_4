574e3c5ce305226835039e01aa1598ec4db64e95
JungTag <dbsdudxor121@naver.com>
2020-07-23 12:23:47 +0900

    Add post DB

--- proj/post/models.py
+++ proj/post/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
+from accounts.models import Signup
 # Create your models here.
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='images/')
+    body = models.TextField()
+    comment = models.ForeignKey(Coment, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    body = models.TextField()
+    pub_date = models.DateTimeField()
