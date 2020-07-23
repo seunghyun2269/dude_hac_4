ae1b54fb00113b9116c91a4c5f1ef98a903ca850
JungTag <dbsdudxor121@naver.com>
2020-07-23 12:24:53 +0900

    Add post DB 2

--- proj/post/models.py
+++ proj/post/models.py
@@ -13,6 +13,6 @@ class Post(models.Model):
         return self.title
 
 class Comment(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
     pub_date = models.DateTimeField()
